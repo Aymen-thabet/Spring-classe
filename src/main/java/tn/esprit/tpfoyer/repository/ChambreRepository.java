7e50992902da5059e3474ac9622619cd514fad6a
Aymen-thabet <Thabet.aymen1@gmail.com>
2025-02-24 11:58:57 +0100

    S6 service + controller

---
+++ src/main/java/tn/esprit/tpfoyer/repository/ChambreRepository.java
@@ -0,0 +1,11 @@
+package tn.esprit.tpfoyer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tn.esprit.tpfoyer.entity.Chambre;
+import java.util.List;
+@Repository
+public interface ChambreRepository extends JpaRepository<Chambre,Long > {
+
+
+}
